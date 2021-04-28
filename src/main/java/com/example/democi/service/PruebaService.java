5948357d66912fb0415d117bc989464275c512b1
Jose Tlacuilo <tlacuilose@outlook.com>
2021-04-28 08:31:22 -0500

    Initialization.

---
+++ src/main/java/com/example/democi/service/PruebaService.java
@@ -0,0 +1,12 @@
+
+package com.example.democi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PruebaService {
+
+    public double suma(double a,double b){
+        return a+b;
+    }
+}
